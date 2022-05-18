85a243d861e8f72c20bef8181a61503c40816eee
Duilio1982 <105397022+Duilio1982@users.noreply.github.com>
2022-05-18 17:01:50 -0300

    Adjunto ejercicios de la clase 2
    quedo a la espera de la corrección. Saludos.

---
+++ ejercicio_4.py
@@ -0,0 +1,41 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese tres palabras y arme un acrónimo con ellas
+# Si desea puede modificar el código para ingresar más palabras
+
+#Ingrese una palabra mas, elegi AFIP (Agente Federal Ingresos Publicos)
+
+print('Ingrese palabra 1:')
+palabra_1 = str(input())
+
+print('Ingrese palabra 2:')
+palabra_2 = str(input())
+
+print('Ingrese palabra 3:')
+palabra_3 = str(input())
+
+print('Ingrese palabra 4:')
+palabra_4 = str(input())
+
+# De cada palabra debe tomar la primera letra y armar el acrónimo
+# Ejemplo: Alumbrado, barrido y limpieza --> ABL
+# Imprimir el resultado en pantalla
+
+caracter_1 = palabra_1 [0]
+caracter_2 = palabra_2 [0]
+caracter_3 = palabra_3 [0]
+caracter_4 = palabra_4 [0]
+
+print(palabra_1, palabra_2, palabra_3, palabra_4)
+print(caracter_1 + caracter_2 + caracter_3 + caracter_4)
+
+
---
+++ ejercicio_5.py
@@ -0,0 +1,45 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese dos palabras y arme combinaciones con ella
+
+# Tome las palabras Boedo Locura
+
+print('Ingrese palabra 1:')
+palabra_1 = str(input())
+
+print('Ingrese palabra 2:')
+palabra_2 = str(input())
+
+# De la primera palabra tome las primeras tres letras, utilice el operador :
+
+sub_text1 = palabra_1 [:3]
+
+# De la segunda palabra tome las primeras dos letras, utilice el operador :
+
+sub_text2 = palabra_2 [:2]
+
+# Formar una nueva palabra con los recortes solicitados
+
+nueva_palabra = sub_text1 + sub_text2
+
+# Imprima en pantalla los resultados
+
+print('La nueva palabra es', nueva_palabra)
+
+
+
+
+
+
+
+
+
